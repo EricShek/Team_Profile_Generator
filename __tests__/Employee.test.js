881761a8834e2c5d1b97c669dda129ff3207910e
Eric Shek <ericshek23@gmail.com>
2022-07-20 17:40:44 -0400

    add Engineer and Intern Library

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,7 @@
 const Employee = require("../lib/Employee")
 describe("Employee", () => {
     describe("test Employee's properties", () => {
-        it("test all properties", () => {
+        it ("test all properties", () => {
             const employee = new Employee('Phil', 1, 'ploy@yahoo.com')
 
             expect(employee.name).toEqual('Phil')
@@ -15,6 +15,7 @@ describe("Employee", () => {
     describe("test Employee's methods", () => {
         it("test all methods", () => {
             const employee = new Employee('brian', 2, 'brian@yahoo.com')
+
             expect(employee.getName()).toEqual('brian')
             expect(employee.getId()).toEqual(2)
             expect(employee.getEmail()).toEqual('brian@yahoo.com')
